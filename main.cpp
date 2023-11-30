d5136ba4e4003c9373c7cb3d600514f207e593e1
dashuimon <dilixixibiao@gmail.com>
2023-11-30 21:43:41 +0800

    [test] Sum method added

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 
+int Sum(const int value1, const int value2)
+{
+    return value1 + value2;
+}
+
 int main()
 {
     std::cout << "Hello world! My name is dashuimon." << std::endl;
+
+    std::cout << "I can calculate the result of 1 + 1 = "
+	      << Sum(1, 1) << std::endl;
 
     return 0;
 }
